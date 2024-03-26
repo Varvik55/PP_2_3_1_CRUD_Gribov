34cdaba56c5182f74d1dad6949822168ce095c70
gribovkirill <qwertyastilark@mail.ru>
2024-03-26 21:47:42 +0400

    Commit2

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,15 +1,11 @@
 package web;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import web.config.AppConfig;
+import web.repository.UserRepository;
 import web.service.UserServise;
 
 public class Main {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
 
-        UserServise userService = context.getBean(UserServise.class);
-
-    }
 }
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,15 +1,22 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.UserServise;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
+	private final UserServise userServise;
+
+	public HelloController(UserServise userServise) {
+		this.userServise = userServise;
+	}
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -18,7 +25,12 @@ public class HelloController {
 		messages.add("I'm Spring MVC application");
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
+		model.addAttribute("people",userServise.getAllUsers());
+		System.out.println("____________________________________________________");
+		System.out.println(userServise.getAllUsers());
+		System.out.println("____________________________________________________");
 		return "index";
 	}
+
 	
 }
